7646a8277cd958818afdf62f60a0496280e3b128
LeGrandMAG <magmukendi0@gmail.com>
2022-11-10 06:56:37 +0900

    Create mario-more.c

---
+++ pset1/mario-more.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i ++)
+    {
+        for (int k = height-1; k > i; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0 ; j <= i ; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int m = 0; m <= i; m++){
+            printf("#");
+        }
+        printf("\n");
+    }
+
+}

4da5f445a957a8f1c15c7ed2c320b53139502b7e
LeGrandMAG <magmukendi0@gmail.com>
2022-12-03 21:34:35 +0900

    Update volume.c

--- week4/lab4/volume.c
+++ week4/lab4/volume.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
 
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t buffer;
+    int16_t buffer;
     while(fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
